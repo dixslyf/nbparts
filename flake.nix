0371bee9f20516d3ce06099ddcc3f8325dd00bd2
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-07-22 22:21:40 +0800

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:Numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        inherit (pkgs) lib;
+
+        ghc = pkgs.haskell.compiler.ghc984;
+        hls = pkgs.haskell-language-server;
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell (
+            let
+              packages = [
+                ghc
+                pkgs.cabal-install
+                hls
+                pkgs.ormolu
+                pkgs.pkg-config
+                pkgs.zlib
+              ];
+            in
+            {
+              inherit packages;
+              LD_LIBRARY_PATH = lib.makeLibraryPath packages;
+            }
+          );
+        };
+      }
+    );
+}
