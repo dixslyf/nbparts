fd945cf73749b32f5b42a361e82cf88abd792a7c
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-08-08 18:55:46 +0800

    feat(unpack): extract cell media

--- app/Unpack.hs
+++ app/Unpack.hs
@@ -1,10 +1,15 @@
 module Unpack where
 
+import Control.Monad.IO.Class (MonadIO)
 import Data.Text qualified as T
 import Data.Text.IO qualified as TIO
-import Text.Pandoc (Block (Div))
-import Text.Pandoc.Class (runIO)
+import System.Directory (createDirectoryIfMissing)
+import System.FilePath (takeFileName, (-<.>), (<.>), (</>))
+import Text.Pandoc (Block (Div), Inline (Image), Pandoc)
+import Text.Pandoc.Class (PandocMonad, getMediaBag, runIO)
+import Text.Pandoc.Class.IO (writeMedia)
 import Text.Pandoc.Error (handleError)
+import Text.Pandoc.MediaBag (MediaBag, MediaItem (MediaItem), lookupMedia, mediaItems)
 import Text.Pandoc.Options (WriterOptions (writerExtensions), def, pandocExtensions)
 import Text.Pandoc.Readers.Ipynb
 import Text.Pandoc.Walk
@@ -13,12 +18,19 @@ import Text.Pandoc.Writers.Markdown (writeMarkdown)
 unpack :: FilePath -> IO ()
 unpack notebookPath = do
   notebookContents <- TIO.readFile notebookPath
-  result <- runIO $ do
-    doc <- readIpynb def notebookContents
-    let processedDoc = walk filterCellOutputs doc
-    writeMarkdown def {writerExtensions = pandocExtensions} processedDoc
+
+  let outputDirectory = notebookPath <.> "nbparts"
+  createDirectoryIfMissing True outputDirectory
+
+  result <-
+    runIO $
+      do
+        doc <- readIpynb def notebookContents
+        let filteredDoc = walk filterCellOutputs doc
+        processedDoc <- extractMedia outputDirectory "media" filteredDoc
+        writeMarkdown def {writerExtensions = pandocExtensions} processedDoc
   markdown <- handleError result
-  TIO.writeFile (notebookPath <> ".md") markdown
+  TIO.writeFile (outputDirectory </> (takeFileName notebookPath -<.> ".md")) markdown
 
 filterCellOutputs :: Block -> Block
 filterCellOutputs (Div attr@(_, classes, _) blocks)
@@ -31,3 +43,30 @@ filterCellOutputs block = block
 isOutputDiv :: Block -> Bool
 isOutputDiv (Div (_, classes, _) _) = T.pack "output" `elem` classes
 isOutputDiv _block = False
+
+-- Adapted from https://github.com/jgm/pandoc/blob/7639e800c5af85e5ded862a6e218d54489d17bfc/src/Text/Pandoc/Class/IO.hs#L236-L245
+--
+-- Pandoc's `extraMedia` unfortunately rewrites the source for images to always include the output directory as a prefix,
+-- which is problematic since the rewritten paths will always be relative to the current directory (of the running nbparts)
+-- instead of the output markdown file. The implementation below allows specifying a prefix; this prefix is prepended to the
+-- output markdown file path, but not to the image sources.
+extractMedia :: (PandocMonad m, MonadIO m) => FilePath -> FilePath -> Pandoc -> m Pandoc
+extractMedia dirPrefix dir doc = do
+  let outdir = dirPrefix </> dir
+  media <- getMediaBag
+  let items = mediaItems media
+  if null items
+    then return doc
+    else do
+      mapM_ (writeMedia outdir) items
+      return $ walk (adjustImagePath dir media) doc
+
+-- Adapted from https://github.com/jgm/pandoc/blob/7639e800c5af85e5ded862a6e218d54489d17bfc/src/Text/Pandoc/Class/IO.hs#L263-L273
+adjustImagePath :: FilePath -> MediaBag -> Inline -> Inline
+adjustImagePath dir mediabag image@(Image attr alt (src, title)) =
+  case lookupMedia (T.unpack src) mediabag of
+    Nothing -> image
+    Just (MediaItem _ mediaPath _) ->
+      let fullpath = dir </> mediaPath
+       in Image attr alt (T.pack fullpath, title)
+adjustImagePath _ _ inline = inline
