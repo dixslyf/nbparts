f397afebcf8e17fb75bb40648a90881065356b3b
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-08-16 22:12:06 +0800

    refactor: avoid use of pandoc for determining mime type file extension

--- app/Nbparts/Unpack/Mime.hs
+++ app/Nbparts/Unpack/Mime.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Nbparts.Unpack.Mime where
 
 import Crypto.Hash qualified as Hash
@@ -5,11 +7,13 @@ import Data.ByteString (ByteString)
 import Data.ByteString qualified as ByteString
 import Data.Ipynb qualified as Ipynb
 import Data.Map qualified as Map
+import Data.Maybe qualified as Maybe
 import Data.Text (Text)
 import Data.Text qualified as Text
+import Data.Text.Encoding qualified as Text
 import Nbparts.Types qualified as Nbparts
+import Network.Mime qualified as Mime
 import System.FilePath ((</>))
-import Text.Pandoc.MIME qualified as Pandoc.MIME
 
 unembedMimeAttachments :: FilePath -> FilePath -> Ipynb.MimeAttachments -> IO Nbparts.UnembeddedMimeAttachments
 unembedMimeAttachments dirPrefix subdir (Ipynb.MimeAttachments mimeAttachments) =
@@ -31,6 +35,14 @@ unembedMimeData _ _ _ (Ipynb.JsonData value) = pure $ Nbparts.JsonData value
 binaryOutputFileName :: Text -> ByteString -> FilePath
 binaryOutputFileName mimetype bytes =
   show (Hash.hashWith Hash.SHA1 bytes)
-    <> case Pandoc.MIME.extensionFromMimeType mimetype of
+    <> case extensionFromMimeType mimetype of
       Nothing -> ""
       Just ext -> "." <> Text.unpack ext
+
+extensionFromMimeType :: Text -> Maybe Text
+extensionFromMimeType "text/plain" = Just "txt"
+extensionFromMimeType "image/tiff" = Just "tiff"
+extensionFromMimeType "image/jpeg" = Just "jpg"
+extensionFromMimeType mt = do
+  exts <- Map.lookup (Text.encodeUtf8 mt) Mime.defaultExtensionMap
+  Maybe.listToMaybe exts
