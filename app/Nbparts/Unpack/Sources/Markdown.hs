0bc26071b04261b0fed3ec721c8536f8a957d1f3
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-08-18 18:24:29 +0800

    refactor(unpack): include original cell attachment URLs when exporting sources to markdown

--- app/Nbparts/Unpack/Sources/Markdown.hs
+++ app/Nbparts/Unpack/Sources/Markdown.hs
@@ -3,16 +3,38 @@
 module Nbparts.Unpack.Sources.Markdown where
 
 import CMarkGFM qualified
-import Control.Monad (join)
 import Control.Monad.Error.Class (MonadError (throwError))
+import Data.Aeson qualified as Aeson
 import Data.Bifunctor qualified
+import Data.ByteString.Lazy qualified as LazyByteString
+import Data.Foldable (foldl')
 import Data.Map (Map)
 import Data.Map qualified as Map
 import Data.Maybe qualified as Maybe
 import Data.Text (Text)
 import Data.Text qualified as Text
+import Data.Text.Encoding qualified as Text
+import GHC.Generics (Generic)
 import Nbparts.Types qualified as Nbparts
 import Nbparts.Unpack.Error qualified as Nbparts
+
+data CellInfo = CellInfo
+  { cellId :: Text,
+    cellType :: Nbparts.CellType,
+    attachmentUrls :: Maybe CellAttachmentUrls
+  }
+  deriving (Generic, Show)
+
+instance Aeson.ToJSON CellInfo
+
+instance Aeson.FromJSON CellInfo
+
+newtype CellAttachmentUrls = CellAttachmentUrls (Map Text Text)
+  deriving (Generic, Show)
+
+instance Aeson.ToJSON CellAttachmentUrls
+
+instance Aeson.FromJSON CellAttachmentUrls
 
 sourcesToMarkdown :: Text -> [Nbparts.Source] -> Either Nbparts.UnpackError Text
 sourcesToMarkdown lang sources = do
@@ -24,16 +46,26 @@ sourceToMarkdown _ (Nbparts.Source cellType@Nbparts.Markdown cellId source maybe
   -- TODO: Investigate options and extensions.
   let mdText = Text.concat source
   let mdTree = CMarkGFM.commonmarkToNode [CMarkGFM.optSourcePos] [] mdText
-  attachmentFixes <- case maybeAttachments of
-    Just attachments -> collectAttachmentFixes cellId attachments mdTree
-    Nothing -> pure []
-  let fixedMdText = applyAttachmentFixes attachmentFixes mdText
-  pure $ cellStart cellId cellType <> "\n" <> fixedMdText
-sourceToMarkdown _ (Nbparts.Source cellType@(Nbparts.Heading _) cellId source _) = pure $ cellStart cellId cellType <> "\n" <> Text.concat source
-sourceToMarkdown _ (Nbparts.Source cellType@Nbparts.Raw cellId source _) = pure $ cellStart cellId cellType <> "\n" <> Text.concat source
+  (fixedMdText, maybeAttachmentUrls) <- case maybeAttachments of
+    Just attachments -> do
+      (attachmentFixes, attachmentUrls@(CellAttachmentUrls innerAttachmentUrls)) <- collectAttachmentFixes cellId attachments mdTree
+      let fixedMdText = applyAttachmentFixes attachmentFixes mdText
+      pure (fixedMdText, if Map.null innerAttachmentUrls then Nothing else Just attachmentUrls)
+    Nothing -> pure (mdText, Nothing)
+  pure $ cellStart (CellInfo cellId cellType maybeAttachmentUrls) <> "\n" <> fixedMdText
+sourceToMarkdown _ (Nbparts.Source cellType@(Nbparts.Heading _) cellId source _) =
+  pure $
+    cellStart (CellInfo cellId cellType Nothing)
+      <> "\n"
+      <> Text.concat source
+sourceToMarkdown _ (Nbparts.Source cellType@Nbparts.Raw cellId source _) =
+  pure $
+    cellStart (CellInfo cellId cellType Nothing)
+      <> "\n"
+      <> Text.concat source
 sourceToMarkdown lang (Nbparts.Source cellType@Nbparts.Code cellId source _) =
   pure $
-    cellStart cellId cellType
+    cellStart (CellInfo cellId cellType Nothing)
       <> "\n"
       <> "```"
       <> lang
@@ -41,20 +73,21 @@ sourceToMarkdown lang (Nbparts.Source cellType@Nbparts.Code cellId source _) =
       <> Text.concat source
       <> "\n```"
 
-cellStart :: Text -> Nbparts.CellType -> Text
-cellStart cellId cellType =
+cellStart :: CellInfo -> Text
+cellStart cellInfo =
   Text.intercalate
     " "
     [ "<!--",
-      "nbparts:begin-cell",
-      "cell-id:" <> cellId,
-      "cell-type:" <> case cellType of
-        Nbparts.Markdown -> "markdown"
-        Nbparts.Heading headingLevel -> "heading-" <> Text.show headingLevel
-        Nbparts.Raw -> "raw"
-        Nbparts.Code -> "code",
+      "nbparts:cell",
+      escapeCellInfo $ Text.decodeUtf8 $ LazyByteString.toStrict $ Aeson.encode cellInfo,
       "-->"
     ]
+
+escapeCellInfo :: Text -> Text
+escapeCellInfo = Text.replace "-->" "-\\->" . Text.replace "\\" "\\\\"
+
+unescapeCellInfo :: Text -> Text
+unescapeCellInfo = Text.replace "\\\\" "\\" . Text.replace "-\\->" "-->"
 
 applyAttachmentFixes :: [(CMarkGFM.PosInfo, Text)] -> Text -> Text
 applyAttachmentFixes attachmentFixes mdText =
@@ -67,7 +100,11 @@ applyAttachmentFixes attachmentFixes mdText =
 -- (which would modify some of the formatting), we collect the positions of the image links
 -- and what to replace them by before performing the replacement using plain text manipulation.
 -- That way, we leave everything else untouched.
-collectAttachmentFixes :: Text -> Nbparts.UnembeddedMimeAttachments -> CMarkGFM.Node -> Either Nbparts.UnpackError [(CMarkGFM.PosInfo, Text)]
+collectAttachmentFixes ::
+  Text ->
+  Nbparts.UnembeddedMimeAttachments ->
+  CMarkGFM.Node ->
+  Either Nbparts.UnpackError ([(CMarkGFM.PosInfo, Text)], CellAttachmentUrls)
 collectAttachmentFixes
   cellId
   (Nbparts.UnembeddedMimeAttachments attachments)
@@ -96,8 +133,14 @@ collectAttachmentFixes
 
         let fixedImageNode = CMarkGFM.Node Nothing (CMarkGFM.IMAGE attachmentUrl title) children
         let fixedImageNodeText = Text.strip $ CMarkGFM.nodeToCommonmark [] Nothing fixedImageNode
-        pure [(posInfo, fixedImageNodeText)]
-collectAttachmentFixes cellId attachments (CMarkGFM.Node _ _ children) = join <$> traverse (collectAttachmentFixes cellId attachments) children
+        pure ([(posInfo, fixedImageNodeText)], CellAttachmentUrls $ Map.singleton attachmentUrl url)
+collectAttachmentFixes cellId attachments (CMarkGFM.Node _ _ children) =
+  foldl'
+    (\(ps, CellAttachmentUrls m) (accPs, CellAttachmentUrls accM) -> (ps ++ accPs, CellAttachmentUrls $ Map.union m accM))
+    ([], CellAttachmentUrls Map.empty)
+    <$> unmergedFixes
+  where
+    unmergedFixes = traverse (collectAttachmentFixes cellId attachments) children
 
 lookupByKeyPrefix :: Text -> Map Text v -> Maybe v
 lookupByKeyPrefix prefix m =
@@ -121,6 +164,7 @@ lineColToIndex textLines line col =
       charsBefore = sum (map ((+ 1) . Text.length) linesBefore)
    in charsBefore + (col - 1)
 
+-- NOTE: Assumes indices do not overlap.
 replaceSlices :: Text -> [((Int, Int), Text)] -> Text
 replaceSlices input = go 0
   where
