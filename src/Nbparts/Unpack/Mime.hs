8df2f28665d8e3184d112b1880992e4de19cc480
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-08-29 15:48:38 +0800

    refactor(unpack): generate shorter attachment names
    The implementation now uses a base64url-encoded SHA-256 hash.
    The attachment name is prepended to the bytes before hashing
    so that the generated hash is different for different attachments
    with the same content.

--- src/Nbparts/Unpack/Mime.hs
+++ src/Nbparts/Unpack/Mime.hs
@@ -1,8 +1,10 @@
 module Nbparts.Unpack.Mime where
 
 import Crypto.Hash qualified as Hash
+import Data.ByteArray qualified as ByteArray
 import Data.ByteString (ByteString)
-import Data.ByteString qualified as ByteString
+import Data.ByteString.Base64.URL qualified as Base64.URL
+import Data.ByteString.Char8 qualified as ByteString
 import Data.Coerce (coerce)
 import Data.Ipynb qualified as Ipynb
 import Data.Map qualified as Map
@@ -19,37 +21,54 @@ unembedMimeAttachments dirPrefix subdir =
   coerce $
     fmap Nbparts.UnembeddedMimeAttachments
       . Map.traverseWithKey
-        (\attName bundle -> unembedMimeBundleWithPrefix (Text.unpack attName <> "-") dirPrefix subdir bundle)
+        (\attName bundle -> unembedMimeBundleWith (genFileName attName) dirPrefix subdir bundle)
+  where
+    -- Include the attachment name in the bytes before hashing so that
+    -- different attachments with the same content have different hashes.
+    -- This is needed so that we know which attachment to reference when
+    -- repacking the notebook.
+    genFileName :: Text -> Text -> ByteString -> FilePath
+    genFileName attName mimeType bytes = binaryOutputFileName mimeType (Text.encodeUtf8 attName <> bytes)
 
-unembedMimeBundleWithPrefix :: String -> FilePath -> FilePath -> Ipynb.MimeBundle -> IO Nbparts.UnembeddedMimeBundle
-unembedMimeBundleWithPrefix fpPrefix dirPrefix subdir =
+unembedMimeBundleWith :: (Text -> ByteString -> FilePath) -> FilePath -> FilePath -> Ipynb.MimeBundle -> IO Nbparts.UnembeddedMimeBundle
+unembedMimeBundleWith genFileName dirPrefix subdir =
   coerce $
     fmap Nbparts.UnembeddedMimeBundle
       . Map.traverseWithKey
-        (unembedMimeDataWithPrefix fpPrefix dirPrefix subdir)
+        (unembedMimeDataWith genFileName dirPrefix subdir)
 
 unembedMimeBundle :: FilePath -> FilePath -> Ipynb.MimeBundle -> IO Nbparts.UnembeddedMimeBundle
-unembedMimeBundle = unembedMimeBundleWithPrefix ""
+unembedMimeBundle = unembedMimeBundleWith binaryOutputFileName
 
-unembedMimeDataWithPrefix :: String -> FilePath -> FilePath -> Ipynb.MimeType -> Ipynb.MimeData -> IO Nbparts.UnembeddedMimeData
-unembedMimeDataWithPrefix fpPrefix dirPrefix subdir mimetype (Ipynb.BinaryData bytes) = do
-  let filename = fpPrefix <> binaryOutputFileName mimetype bytes
+unembedMimeDataWith ::
+  (Text -> ByteString -> FilePath) ->
+  FilePath ->
+  FilePath ->
+  Ipynb.MimeType ->
+  Ipynb.MimeData ->
+  IO Nbparts.UnembeddedMimeData
+unembedMimeDataWith genFileName dirPrefix subdir mimetype (Ipynb.BinaryData bytes) = do
+  let filename = genFileName mimetype bytes
+
   let relPath = subdir </> filename
   let writePath = dirPrefix </> relPath
   ByteString.writeFile writePath bytes
   return $ Nbparts.BinaryData relPath
-unembedMimeDataWithPrefix _ _ _ _ (Ipynb.TextualData text) = pure $ Nbparts.TextualData text
-unembedMimeDataWithPrefix _ _ _ _ (Ipynb.JsonData value) = pure $ Nbparts.JsonData value
+unembedMimeDataWith _ _ _ _ (Ipynb.TextualData text) = pure $ Nbparts.TextualData text
+unembedMimeDataWith _ _ _ _ (Ipynb.JsonData value) = pure $ Nbparts.JsonData value
 
 unembedMimeData :: FilePath -> FilePath -> Ipynb.MimeType -> Ipynb.MimeData -> IO Nbparts.UnembeddedMimeData
-unembedMimeData = unembedMimeDataWithPrefix ""
+unembedMimeData = unembedMimeDataWith binaryOutputFileName
 
 binaryOutputFileName :: Text -> ByteString -> FilePath
 binaryOutputFileName mimetype bytes =
-  show (Hash.hashWith Hash.SHA1 bytes)
+  digestToBase64 (Hash.hashWith Hash.SHA256 bytes)
     <> case extensionFromMimeType mimetype of
       Nothing -> ""
       Just ext -> "." <> Text.unpack ext
+  where
+    digestToBase64 :: Hash.Digest a -> String
+    digestToBase64 digest = ByteString.unpack . Base64.URL.encode $ ByteArray.convert digest
 
 extensionFromMimeType :: Text -> Maybe Text
 extensionFromMimeType "text/plain" = Just "txt"
