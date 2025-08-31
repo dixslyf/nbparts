923ea17828985fdbe5815c26e1ddb259497cd80d
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-08-31 18:07:12 +0800

    refactor: add markdown AST types in preparation for switching to `commonmark` package

---
+++ src/Nbparts/Types/Sources/Markdown.hs
@@ -0,0 +1,168 @@
+module Nbparts.Types.Sources.Markdown where
+
+import Commonmark
+  ( Format,
+    HasAttributes,
+    IsBlock
+      ( blockQuote,
+        codeBlock,
+        heading,
+        list,
+        paragraph,
+        plain,
+        rawBlock,
+        referenceLinkDefinition,
+        thematicBreak
+      ),
+    IsInline
+      ( code,
+        emph,
+        entity,
+        escapedChar,
+        image,
+        lineBreak,
+        link,
+        rawInline,
+        softBreak,
+        str,
+        strong
+      ),
+    ListSpacing,
+    ListType,
+    Rangeable (ranged),
+    SourceRange, ToPlainText (toPlainText),
+  )
+import Commonmark.Types (HasAttributes (addAttributes))
+import Data.Map (Map)
+import Data.Map qualified as Map
+import Data.Sequence (Seq)
+import Data.Sequence qualified as Seq
+import Data.Text (Text)
+import Data.Data (Data, Typeable)
+import qualified Data.Data as Data
+
+newtype Inlines = Inlines (Seq Inline)
+  deriving (Show, Semigroup, Monoid, Data, Typeable)
+
+data Inline
+  = Inline
+  { inlineType :: InlineType,
+    sourceRange :: SourceRange,
+    attributes :: Map Text Text
+  }
+  deriving (Show, Data, Typeable)
+
+data InlineType
+  = Code Text
+  | Emph Inlines
+  | Entity Text
+  | EscapedChar Char
+  | Image Text Text Inlines
+  | LineBreak
+  | Link Text Text Inlines
+  | RawInline Format Text
+  | SoftBreak
+  | Str Text
+  | Strong Inlines
+  deriving (Show, Data, Typeable)
+
+singletonInlines :: InlineType -> Inlines
+singletonInlines = Inlines . Seq.singleton . mkInline
+
+mkInline :: InlineType -> Inline
+mkInline ilType = Inline ilType mempty mempty
+
+instance Rangeable Inlines where
+  ranged srcRange (Inlines ils) = Inlines $ fmap (\(Inline ilType _ attrs) -> Inline ilType srcRange attrs) ils
+
+instance HasAttributes Inlines where
+  addAttributes attrs (Inlines ils) =
+    Inlines $
+      fmap
+        (\(Inline ilType srcRange ilAttrs) -> Inline ilType srcRange $ ilAttrs <> Map.fromList attrs)
+        ils
+
+instance IsInline Inlines where
+  lineBreak = singletonInlines LineBreak
+  softBreak = singletonInlines SoftBreak
+  str = singletonInlines . Str
+  entity = singletonInlines . Entity
+  escapedChar = singletonInlines . EscapedChar
+  emph = singletonInlines . Emph
+  strong = singletonInlines . Strong
+  link target title ils = singletonInlines $ Link target title ils
+  image target title ils = singletonInlines $ Image target title ils
+  code = singletonInlines . Code
+  rawInline format txt = singletonInlines $ RawInline format txt
+
+instance ToPlainText Inlines where
+  toPlainText (Inlines ils) = foldMap toPlainText ils
+
+instance ToPlainText Inline where
+  toPlainText (Inline ilType _srcRange _attrs) = toPlainText ilType
+
+instance ToPlainText InlineType where
+  toPlainText (Code txt) = ""
+  toPlainText (Emph ils) = ""
+  toPlainText (Entity txt) = ""
+  toPlainText (EscapedChar char) = ""
+  toPlainText (Image target title ils) = ""
+  toPlainText LineBreak = "\n"
+  toPlainText (Link target title ils) = ""
+  toPlainText (RawInline fmt txt) = ""
+  toPlainText SoftBreak = "\n"
+  toPlainText (Str txt) = ""
+  toPlainText (Strong ils) = ""
+
+newtype Blocks = Blocks (Seq Block)
+  deriving (Show, Semigroup, Monoid, Data, Typeable)
+
+data Block = Block
+  { blockType :: BlockType,
+    sourceRange :: SourceRange,
+    attributes :: Map Text Text
+  }
+  deriving (Show, Data, Typeable)
+
+data BlockType
+  = Paragraph Inlines
+  | Plain Inlines
+  | ThematicBreak
+  | BlockQuote Blocks
+  | CodeBlock Text Text
+  | Heading Int Inlines
+  | RawBlock Format Text
+  | ReferenceLinkDefinition Text (Text, Text)
+  | List ListType ListSpacing Blocks
+  deriving (Show, Data, Typeable)
+
+singletonBlocks :: BlockType -> Blocks
+singletonBlocks = Blocks . Seq.singleton . mkBlock
+
+mkBlock :: BlockType -> Block
+mkBlock blockType = Block blockType mempty mempty
+
+instance Rangeable Blocks where
+  ranged srcRange (Blocks blks) =
+    Blocks $
+      fmap
+        (\(Block blkType _ attrs) -> Block blkType srcRange attrs)
+        blks
+
+instance HasAttributes Blocks where
+  addAttributes attrs (Blocks blks) =
+    Blocks $
+      fmap
+        (\(Block blkType srcRange blkAttrs) -> Block blkType srcRange $ blkAttrs <> Map.fromList attrs)
+        blks
+
+instance IsBlock Inlines Blocks where
+  paragraph = singletonBlocks . Paragraph
+  plain = singletonBlocks . Plain
+  thematicBreak = singletonBlocks ThematicBreak
+  blockQuote = singletonBlocks . BlockQuote
+  codeBlock info txt = singletonBlocks $ CodeBlock info txt
+  heading level ils = singletonBlocks $ Heading level ils
+  rawBlock format txt = singletonBlocks $ RawBlock format txt
+  referenceLinkDefinition a b = singletonBlocks $ ReferenceLinkDefinition a b
+  list listType listSpacing blks = singletonBlocks $ List listType listSpacing (mconcat blks)
