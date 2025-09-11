157a039de89d82a61bcfbf4665a5dc43a8fff3d4
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-09-11 12:05:24 +0800

    feat(flake): build with haskell.nix
    Includes cross-compilation support for MacOS and Windows builds, though
    untested.

--- flake.nix
+++ flake.nix
@@ -1,57 +1,84 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:Numtide/flake-utils";
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskell-nix/nixpkgs-unstable";
+  };
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    allow-import-from-derivation = "true";
   };
 
   outputs =
     {
       nixpkgs,
       flake-utils,
+      haskell-nix,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit (pkgs) lib;
+        pkgs = import nixpkgs {
+          inherit
+            system
+            overlays
+            ;
+          inherit (haskell-nix) config;
+        };
 
-        ghc = pkgs.haskell.compiler.ghc984;
-        hls = pkgs.haskell-language-server;
+        overlays = [
+          haskell-nix.overlay
+          (final: _prev: {
+            nbparts = final.haskell-nix.project' {
+              src = ./.;
+
+              compiler-nix-name = "ghc984";
+
+              shell = {
+                tools = {
+                  cabal = { };
+                  haskell-language-server = { };
+                };
+
+                buildInputs = with pkgs; [
+                  # https://github.com/input-output-hk/haskell.nix/issues/1776
+                  (pkgs.writeScriptBin "haskell-language-server-wrapper" ''
+                    #!${pkgs.stdenv.shell}
+                    exec haskell-language-server "$@"
+                  '')
+
+                  pkgs.ormolu
+
+                  (pkgs.python3.withPackages (
+                    ps: with ps; [
+                      jupyterlab
+                      matplotlib
+                      pillow
+                      ipympl
+                    ]
+                  ))
+
+                  pkgs.nodePackages.json-diff
+                ];
+              };
+            };
+          })
+        ];
+
+        flake = pkgs.nbparts.flake {
+          crossPlatforms = ps: [
+            ps.ucrt64
+            ps.x86_64-darwin
+          ];
+        };
       in
-      {
-        devShells = {
-          default = pkgs.mkShell (
-            let
-              packages = [
-                ghc
-                pkgs.cabal-install
-                hls
-                pkgs.ormolu
-                pkgs.pkg-config
-                pkgs.zlib
-
-                (pkgs.python3.withPackages (
-                  ps: with ps; [
-                    jupyterlab
-                    matplotlib
-                    pillow
-                    ipympl
-                  ]
-                ))
-
-                pkgs.nodePackages.json-diff
-              ];
-            in
-            {
-              inherit packages;
-              PKG_CONFIG_PATH = "${pkgs.zlib.dev}/lib/pkgconfig";
-              C_INCLUDE_PATH = "${pkgs.zlib.dev}/include";
-              LD_LIBRARY_PATH = lib.makeLibraryPath [
-                pkgs.zlib
-              ];
-            }
-          );
+      flake
+      // {
+        packages = flake.packages // {
+          default = flake.packages."nbparts:exe:nbparts";
         };
       }
     );
